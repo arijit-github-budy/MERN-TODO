e38d33f2bf710a29837794e7e45d53ba0a450670
arijit-roy_1997 <arijit.roy@alliedmedia.com>
2024-06-24 00:14:07 +0530

    backend is ready for live.

--- client/src/global/states/reducers/auth/auth.types.js
+++
@@ -1,3 +0,0 @@
-export const LOGIN = '[AUTH] LOGIN';
-export const REGISTRATION = '[AUTH] REGISTRATION';
-export const LOGOUT = '[AUTH] LOGOUT';
--- client/src/global/states/reducers/todo/todo.types.js
+++
@@ -1,2 +0,0 @@
-export const TODO_LISTS = '[TODO] TODO_LISTS';
-export const SET_EDIT_TODO = '[TODO] SET_EDIT_TODO';
